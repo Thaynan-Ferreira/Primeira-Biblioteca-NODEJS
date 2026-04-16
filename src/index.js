ae513eafe98d028f86d87c99e64a345047cf61ea
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-04-15 21:23:47 -0300

    fix: Separar a contagem por paragrafo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const caminhoArquivo = process.argv; //passo o caminho do arquivo como argumento
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    verificaPalavrasDuplicadas(texto);
+    quebraEmParagrafos(texto);
+    //verificaPalavrasDuplicadas(texto);
 })
 
 // criar um array com as palavras
@@ -16,6 +17,15 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
 //     "javascript": 2
 // }
 
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n'); //quebra o texto em parágrafos, usando a quebra de linha como separador, e armazena em um array chamado paragrafos
+    const contagem = paragrafos.map((paragrafo) => {
+        return verificaPalavrasDuplicadas(paragrafo); //para cada parágrafo, chama a função verificaPalavrasDuplicadas e retorna o resultado, que é um objeto com a contagem de palavras para aquele parágrafo
+    });
+    console.log(contagem);
+
+}
+
 function verificaPalavrasDuplicadas(texto) {
     const listaPalavras = texto.split(' '); //quebra o texto em palavras, usando o espaço como separador, e armazena em um array chamado listaPalavras
     const resultado = {};
@@ -23,5 +33,5 @@ function verificaPalavrasDuplicadas(texto) {
     listaPalavras.forEach(palavra => {
         resultado[palavra] = (resultado[palavra] || 0) + 1; //se a palavra já existe no resultado, pega o valor atual e soma 1, se não existe, atribui o valor 0 e soma 1
     });
-    console.log(resultado);
+    return resultado;
 }
